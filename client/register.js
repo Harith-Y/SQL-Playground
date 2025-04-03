ae9af522d9c90cb44f4705817e3ce7df572ab154
Harith Yerragolam <yharith16@gmail.com>
2025-04-03 22:48:28 +0530

    Add Firebase integration and update dependencies in package.json

---
+++ client/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgJBK_ixbBUhLwuPKRMoXkCuUCscX-_Jg",
+  authDomain: "sql-playground-e2486.firebaseapp.com",
+  projectId: "sql-playground-e2486",
+  storageBucket: "sql-playground-e2486.firebasestorage.app",
+  messagingSenderId: "709069034921",
+  appId: "1:709069034921:web:190d4caca19621a91120b8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
